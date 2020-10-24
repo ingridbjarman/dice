a64381c2952e8e15bb74764a04d0f0bacb3f5b74
ingrid <bjarmaningrid@gmail.com>
2020-10-24 18:14:38 +0100

    array functionality is not useful, deleted. Testing default value for initialisation

--- lib/dice.rb
+++ lib/dice.rb
@@ -5,17 +5,11 @@ class Dice
   attr_reader :dice_range
 
   def initialize(number_of_sides=6)
-    @roll_results = []
     @dice_range = (1..number_of_sides).to_a
   end
 
   def roll
     @roll_result = @dice_range.sample
-    @roll_results.push(roll_result)
-    return roll_result
   end
 
-  def restart
-    @roll_results = []
-  end
 end
--- spec/dice_spec.rb
+++ spec/dice_spec.rb
@@ -3,8 +3,8 @@ require "dice"
 describe 'dice' do
   subject { Dice.new() }
   describe 'initialization' do
-    it 'should start with an empty array of roll results' do
-      expect(subject.roll_results).to be_empty
+    it 'has a default dice range of 1-6' do
+      expect(subject.dice_range.last).to eq(6)
     end
   end
 
@@ -13,21 +13,6 @@ describe 'dice' do
       subject.roll
       expect(subject.roll_result).to be_between(1, subject.dice_range.last)
     end
-    it 'when rolled, the roll result is stored in the array of results' do
-      subject.roll
-      expect(subject.roll_results).to include(subject.roll_result)
-    end
-    it 'when rolled multiple times, each result should be stored in the results array' do
-      result1 = subject.roll
-      result2 = subject.roll
-      expect(subject.roll_results).to include(result1, result2)
-    end
   end
 
-  describe 'restart' do
-    it 'upon restart, the array of roll results is emptied' do
-      subject.restart
-      expect(subject.roll_results).to be_empty
-    end
-  end
 end
